fb8a92823c2446019314cdb4564119f3729691a7
KasperAPedersen <35560445+KasperAPedersen@users.noreply.github.com>
2020-12-10 04:59:19 +0100

    Update
    Added alert functionality.
    Added user logged in checks.
    Added 'add stock' functionality.
    Added 'update stocks' functionality.

--- public/inc/styling/js/watchlist.js
+++ public/inc/styling/js/watchlist.js
@@ -1,8 +1,28 @@
+// -- Config variables
 let url = "localhost:3000";
-//
+let alertTime = 10000; // How long should the alert show (in ms)
+
+// -- Operation variables
+let currentAlert = 0;
 document.addEventListener('DOMContentLoaded', () => {
-    
+    setInterval(checkUser(), 10000); checkUser();
 });
+
+function checkUser(){
+    fetch(`http://localhost:3000/watchlist`)
+    .then((res) => {
+        if(res.status !== 200) {
+            throw new Error("Something went wrong!");
+        } else {
+            return res.json();
+        }
+    })
+    .then((json) => {
+        if(json.loggedIn == 0) {
+            window.location.href = `http://${url}/login.html`;
+        }
+    })
+}
 
 // name, currency, marked, price, marked status, day high, day low, currency symbol
 let stocks = [];
@@ -10,7 +30,7 @@ let stocks = [];
 document.getElementById('submitBtn').addEventListener('click', () => {
     let stockName = document.getElementById('stockName').value;
     if(stockName != undefined) {
-        fetch(`./stock/add?name=${stockName}`)
+        fetch(`/stocks/add?name=${stockName}`)
         .then((res) => {
             if(res.status !== 200) {
                 throw new Error("Failure 1");
@@ -19,49 +39,55 @@ document.getElementById('submitBtn').addEventListener('click', () => {
             }
         })
         .then((json) => {
-            if(json.status > 0) {
-                stocks.push([stockName, "DKK", "Nasdaq", 0, "åben", 1.0, 0.1, "DKK"]);
-                let elem = document.createElement('div');
-                elem.classList = 'stockCard';
-                elem.innerHTML = `<h3 id="stock${stocks.length-1}" class="cardStockName">Stock Navn</h3><p id="stock${stocks.length-1}Price" class="cardStockPrice">00.00</p><p id="stock${stocks.length-1}MarketStatus" class="cardStockMarketStatus">Åben</p><p id="stock${stocks.length-1}Market" class="cardStockMarket">Xetra</p><p id="stock${stocks.length-1}PriceHigh" class="cardStockPriceHigh">1.0</p><p id="stock${stocks.length-1}PriceLow" class="cardStockPriceLow">0.1</p><div class="floatFixer"></div>`;
-                document.getElementById('stocks').appendChild(elem);
+            if(json.status == 1) {
+                alert('success', 'Stock Added!');
+                update();
+            } else {
+                alert('failure', 'Something went wrong!');
             }
         })
     }
 });
 
+function update(){
+    fetch(`/stocks/update`)
+        .then((res) => {
+            if(res.status !== 200) {
+                throw new Error("Failure 1");
+            } else {
+                return res.json();
+            }
+        })
+        .then((json) => {
+            for(stock of json) {
+                let elem = document.createElement('div');
+                elem.classList = 'stockCard';
+                elem.innerHTML = `<h3 id="stock${stock[8]}" class="cardStockName">${stock[0]}</h3><p id="stock${stock[8]}Price" class="cardStockPrice">${stock[3]}</p><p id="stock${stock[8]}MarketStatus" class="cardStockMarketStatus">${stock[4]}</p><p id="stock${stock[8]}Market" class="cardStockMarket">${stock[2]}</p><p id="stock${stock[8]}PriceHigh" class="cardStockPriceHigh">${stock[5]}</p><p id="stock${stock[8]}PriceLow" class="cardStockPriceLow">${stock[6]}</p><div class="floatFixer"></div>`;
+                document.getElementById('stocks').appendChild(elem);
+            }
+        })
+}
+
 document.getElementById('updateBtn').addEventListener('click', () => {
     // Update stocks with this button
-    fetch(`./update/all`)
-    .then((res) => {
-        if(res.status !== 200) {
-            throw new Error("Failure 1");
-        } else {
-            return res.json();
-        }
-        
-    })
-    .then((json) => {
-        if(json.length > 0) {
-            for(const [index, key] of json.entries()) {
-                // name, currency, marked, price, marked status, day high, day low, currency symbol
-                if(stocks[index] != undefined) {
-                    stocks[index][1] = key[1];
-                    stocks[index][2] = key[2];
-                    stocks[index][3] = key[3];
-                    stocks[index][4] = key[4];
-                    stocks[index][5] = key[5];
-                    stocks[index][6] = key[6];
-                    stocks[index][7] = key[7];
-                    
-                    document.getElementById(`stock${index}`).innerHTML = stocks[index][0];
-                    document.getElementById(`stock${index}Price`).innerHTML = `${stocks[index][3].toString()} ${stocks[index][1].toString()}`;
-                    document.getElementById(`stock${index}MarketStatus`).innerHTML = `${(stocks[index][4] == "CLOSED" ? "Lukket" : "Åben")}`;
-                    document.getElementById(`stock${index}Market`).innerHTML = stocks[index][2];
-                    document.getElementById(`stock${index}PriceHigh`).innerHTML = `<i class="fas fa-arrow-circle-up"></i> ${stocks[index][5].toString()} ${stocks[index][7].toString()}`;
-                    document.getElementById(`stock${index}PriceLow`).innerHTML = `<i class="fas fa-arrow-circle-down"></i> ${stocks[index][6].toString()} ${stocks[index][7].toString()}`;
-                }
-            }
-        }
-    })
+    let cards = document.getElementsByClassName('stockCard');
+    for(card of cards) {
+        card.remove();
+    }
+    update();
 });
+
+function alert(type, msg) {
+    if(msg != undefined && msg != "" && type != undefined && type != "") {
+        let tmp = currentAlert;
+        let elem = document.createElement('p');
+        elem.classList = (type == "success" ? "alertMsg2" : "alertMsg1");
+        elem.innerHTML = msg;
+        elem.id = `alert${tmp}`;
+        document.getElementById('alert').appendChild(elem);
+        setTimeout(() => {
+            document.getElementById(`alert${tmp}`).remove();
+            currentAlert++;
+        }, alertTime);
+    }
+}
