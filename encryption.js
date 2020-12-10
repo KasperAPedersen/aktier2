46d0fa773674b3c7ed1ceaede64530e60a78c374
KasperAPedersen <35560445+KasperAPedersen@users.noreply.github.com>
2020-12-10 04:56:44 +0100

    Update
    Changed encryption module to use bcrypt.

--- encryption.js
+++ encryption.js
@@ -1,19 +1,24 @@
-let crypto = require('crypto');
-let secret = "ThWmZq3t6w9z$C&F)J@NcRfUjXn2r5u7"; // Secret Key 256 bit
-let initializationVector = crypto.randomBytes(16); // IV 16 bytes
+let bcrypt = require('bcrypt');
+
+let saltRounds = 10;
 
 module.exports = {
-    encrypt: (textToEncrypt) => {
-        let cipher = crypto.createCipheriv('aes-256-ctr', secret, initializationVector);
-        let encrypted = Buffer.concat([cipher.update(textToEncrypt), cipher.final()]);
-        return {
-            initializationVector: initializationVector.toString('hex'),
-            content: encrypted.toString('hex')
-        };
+    encrypt: (pass, callback) => {
+        bcrypt.hash(pass, 10, (err, hash) => {
+            if(err) {
+                console.log(err);
+            } else {
+                callback(hash);
+            }
+        });
     },
-    decrypt: (hash) => {
-        let decipher = crypto.createDecipheriv('aes-256-ctr', secret, Buffer.from(hash.initializationVector, 'hex'));
-        let decrypted = Buffer.concat([decipher.update(Buffer.from(hash.content, 'hex')), decipher.final()]);
-        return decrypted.toString();
+    compare: (pass, hash, callback) => {
+        bcrypt.compare(pass, hash, (err, res) => {
+            if(err) {
+                console.log(err);
+            } else {
+                callback(res);
+            }
+        });
     }
 }
